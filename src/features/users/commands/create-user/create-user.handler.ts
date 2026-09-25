d077875fca6d0c8eb09a5cfb8c59be4edb2fcf21
Gab <avasquezu@ingenieria.com>
2026-09-25 17:38:33 -0500

    Se crea el usuario con el handler

--- src/features/users/commands/create-user/create-user.handler.ts
+++ src/features/users/commands/create-user/create-user.handler.ts
@@ -1,30 +1,33 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Person } from '../../entities/person.entity.js';
 import { User } from '../../entities/user.entity.js';
+import { UnitOfWork } from '../../../../shared/database/unit-of-work.js';
 import { CreateUserCommand } from './create-user.command.js';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  constructor(
-    @InjectRepository(User)
-    private readonly repository: Repository<User>,
-  ) {}
+  constructor(private readonly unitOfWork: UnitOfWork) {}
 
   execute(command: CreateUserCommand): Promise<User> {
-    return this.repository.manager.transaction(async (manager) => {
-      const person = manager.create(Person, command.data.person);
-      await manager.save(Person, person);
+    return this.unitOfWork.execute(async (manager) => {
+      const {
+        person: personData,
+        nickName,
+        email,
+        passwordHash,
+      } = command.data;
+
+      const person = manager.create(Person, personData);
+      await manager.save(person);
 
       const user = manager.create(User, {
-        nickName: command.data.nickName,
-        email: command.data.email,
-        passwordHash: command.data.passwordHash,
+        nickName,
+        email,
+        passwordHash,
         person,
       });
 
-      return manager.save(User, user);
+      return manager.save(user);
     });
   }
 }
--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UnitOfWork } from '../../shared/database/unit-of-work.js';
 import { CreateUserHandler } from './commands/create-user/create-user.handler.js';
 import { User } from './entities/user.entity.js';
 import { FindAllUsersHandler } from './queries/find-all-users/find-all-users.handler.js';
@@ -8,6 +9,6 @@ import { UsersController } from './users.controller.js';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [CreateUserHandler, FindAllUsersHandler],
+  providers: [CreateUserHandler, FindAllUsersHandler, UnitOfWork],
 })
 export class UsersModule {}
